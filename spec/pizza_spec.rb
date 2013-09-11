779132337ab150f5e5a91075c3cf109222c0ba19
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-11 15:13:13 -0500

    15: adds a failing test to seee if pizza::pie accepts toppings

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -4,7 +4,18 @@ require 'rspec'
 require_relative '../pizza'
 
 describe Pizza::Pie do
-#describe '.initialize'
+  describe '.initialize' do
+    it "should take all the toppings" do
+      toppings = [
+        Pizza::Topping.new('mushroom', vegetarian: true),
+        Pizza::Topping.new('pepperoni')
+      ]
+      pizza =Pizza::pie.new(toppings)
+
+      expect(Pizza.toppings).to eq(toppings)
+       
+    end
+  end
 
 end 
 
@@ -16,7 +27,7 @@ describe Pizza::Topping do
       expect(topping.name).to eq('olives')
     end
 
-    it 'sets weather or not the topping is vegetarian' do
+    it 'sets whether or not the topping is vegetarian' do
       topping = Pizza::Topping.new 'bell peppers', vegetarian: true
 
       expect(topping.vegetarian).to be_true
